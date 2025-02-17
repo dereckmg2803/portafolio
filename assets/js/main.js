98254bdf9695647d98e329aad56c862c68f07512
Dereck <dexcka3@gmail.com>
2025-02-17 11:44:04 -0500

    Funcionalidad cambiar idioma a ingles

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,27 @@ resetToHome();
 updateCurrentYear();
 parallax();
 
+// Detectar si el usuario ya seleccionó un idioma antes
+const savedLanguage = localStorage.getItem("language");
 
+// Si el usuario seleccionó inglés antes, redirigir automáticamente a la página en inglés
+if (savedLanguage === "en" && !window.location.href.includes("index-en.html")) {
+  window.location.href = "index-en.html";
+} else if (savedLanguage === "es" && !window.location.href.includes("index.html")) {
+  window.location.href = "index.html";
+}
+
+// Marcar el checkbox según el idioma guardado
+document.getElementById("language-toggle").checked = savedLanguage === "en";
+
+// Evento para cambiar de idioma
+document.getElementById("language-toggle").addEventListener("change", function () {
+  if (this.checked) {
+    localStorage.setItem("language", "en"); // Guardar idioma en inglés
+    window.location.href = "index-en.html";
+  } else {
+    localStorage.setItem("language", "es"); // Guardar idioma en español
+    window.location.href = "index.html";
+  }
+});
 
